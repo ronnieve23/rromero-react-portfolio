1b779162319cbd074d9a1439b80524cf7cf62759
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-04 17:26:03 -0400

    Updates

---
+++ precache-manifest.45664a7e4e20c3579834ecaee397b20d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f53d82441f8a6437491723411b1daa",
+    "url": "/rromero-react-portfolio/index.html"
+  },
+  {
+    "revision": "a7d2c5d8b6c97e9def57",
+    "url": "/rromero-react-portfolio/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "c307d8c335129a5af738",
+    "url": "/rromero-react-portfolio/static/css/main.3d3670d9.chunk.css"
+  },
+  {
+    "revision": "a7d2c5d8b6c97e9def57",
+    "url": "/rromero-react-portfolio/static/js/2.fba14aac.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/rromero-react-portfolio/static/js/2.fba14aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75730711f86657204aaa",
+    "url": "/rromero-react-portfolio/static/js/3.f112796f.chunk.js"
+  },
+  {
+    "revision": "c307d8c335129a5af738",
+    "url": "/rromero-react-portfolio/static/js/main.9c128c18.chunk.js"
+  },
+  {
+    "revision": "f3d40896c0831efe6797",
+    "url": "/rromero-react-portfolio/static/js/runtime-main.1b962ec5.js"
+  },
+  {
+    "revision": "07d422d1b6752a60157b4067e6a1639d",
+    "url": "/rromero-react-portfolio/static/media/NPT.07d422d1.png"
+  },
+  {
+    "revision": "3d8b5bddbda54caf8fa65236b09eb541",
+    "url": "/rromero-react-portfolio/static/media/WorkDayPlanner.3d8b5bdd.png"
+  },
+  {
+    "revision": "9a213da706aba02acd922e1e138f64b9",
+    "url": "/rromero-react-portfolio/static/media/deepblue.9a213da7.png"
+  },
+  {
+    "revision": "9772780713b3070963469f80f9dd9076",
+    "url": "/rromero-react-portfolio/static/media/notetaker.97727807.png"
+  },
+  {
+    "revision": "31ebdd0288f21edc190c0e977a4ab760",
+    "url": "/rromero-react-portfolio/static/media/ronromero.31ebdd02.jpeg"
+  },
+  {
+    "revision": "dd3282f09d06c0b51f1dbea1c967e917",
+    "url": "/rromero-react-portfolio/static/media/the-tech-blog.dd3282f0.png"
+  },
+  {
+    "revision": "5172901b7470997fee66b34dc95b675c",
+    "url": "/rromero-react-portfolio/static/media/weatherlookup.5172901b.png"
+  }
+]);
