37dde3e5c74e16d9004d7425672339a070ea2e6d
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-04 12:49:56 -0400

    added navbar toggler

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar,Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 
 function Navigation(props) {
     const {
-        sections =[],
+        sections = [],
         setCurrentSection
     } = props;
     return (
-        <Navbar bg="dark" variant="dark" style={{fontFamily:"monospace",fontSize:"Larger"}}>
-            <Container className="container-fluid navfont">
-                <Navbar.Brand style={{fontSize:"Larger"}}>Ron Romero</Navbar.Brand>
-                <Nav className="ml-auto">
-                    <Nav.Link href="#" onClick={() => setCurrentSection(sections[0])}>About Me</Nav.Link>
-                    <Nav.Link href="#" onClick={() => setCurrentSection(sections[1])} >Portfolio</Nav.Link>
-                    <Nav.Link  onClick={() => setCurrentSection(sections[2])} >Contact</Nav.Link>
-                    <Nav.Link href="#" >Resume</Nav.Link>
-                </Nav>
+        <Navbar bg="dark" variant="dark" style={{ fontFamily: "monospace", fontSize: "Larger" }} expand="md" className="ml-auto">
+            <Container className="container-fluid">
+                <Navbar.Brand style={{ fontSize: "Larger" }}>Ron Romero</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse>
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#" onClick={() => setCurrentSection(sections[0])}>About Me</Nav.Link>
+                        <Nav.Link href="#" onClick={() => setCurrentSection(sections[1])} >Portfolio</Nav.Link>
+                        <Nav.Link href="#" onClick={() => setCurrentSection(sections[2])} >Contact</Nav.Link>
+                        <Nav.Link href="#" >Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
