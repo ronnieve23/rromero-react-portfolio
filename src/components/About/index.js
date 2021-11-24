f385508bcf5ae22cac0a240bfb801990982d2629
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-11-23 20:16:28 -0500

    added new project

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import profilePic from '../../assets/img/ronromero.jpeg'
 import '../About/about.css'
 
 function About() {
-    return (
-        <Container className="mb-5" >
-            <Row className= "mt-3">
-                <Col>
-                <img src={profilePic} style={{height: '500px'}} alt="Ron Romero" className="mt-3"></img>
+    return (    
+        <Card className="border-0" style={{ fontFamily: "monospace", backgroundColor: "#AFAFAF" }} >
+            <Row className="no-gutters">
+                <Col className="" align="center">
+                    <Card.Img src={profilePic} className="mt-3" style={{height:"500px", width:"300px"}}></Card.Img>
                 </Col>
-                <Col className="mb-5">
-                <h1>About Me</h1>
-                <p>I currently work as a Technical Support Specialist and I'm currently enrolled in Rutgers Coding Bootcamp hoping to do a career change after the program. This Portfolio showcases the projects we are working on through the course of the Bootcamp.</p>
-                </Col>
-            </Row>
-        </Container>
+            </Row>    
+            <Row>
+            <Col className="mb-5">
+                <Card style={{ fontFamily: "monospace", backgroundColor: "#AFAFAF" }} className="border-0 m-3">
+                    <Card.Body>
+                        <Card.Title style={{fontSize:'50px'}} className="text-center">About Me</Card.Title>
+                        <Card.Text>
+                        I currently work as a Technical Support Specialist and I'm currently enrolled in Rutgers Coding Bootcamp hoping to do a career change after the program. This Portfolio showcases the projects we are working on through the course of the Bootcamp.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            </Row>   
+        </Card>
+         
     )
 }
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,10 +7,7 @@ import deepBlue from '../../assets/img/deepblue.png';
 import techBlog from '../../assets/img/the-tech-blog.png'
 import weather from '../../assets/img/weatherlookup.png'
 import workDayPlanner from '../../assets/img/WorkDayPlanner.png'
-import noteTaker from '../../assets/img/notetaker.png'
-
-
-
+import onlybans from '../../assets/img/OnlyBans.png'
 function Portfolio() {
     return (
         <Container fluid className="mt-2 mb-5">
@@ -81,12 +78,12 @@ function Portfolio() {
                 </Col>
                 <Col className="6">
                     <Card className="border-0">
-                        <Card.Img variant="top" src={noteTaker} className="cardImage"></Card.Img>
+                        <Card.Img variant="top" src={onlybans} className="cardImage"></Card.Img>
                         <Card.Body className="text-center" style={{ fontFamily: "monospace", backgroundColor: "#AFAFAF" }}>
                             <Card.Title style={{ fontSize: "30px" }}>Deep Blue</Card.Title>
                             <Container>
-                                <Button href="https://deep-blue-project-2.herokuapp.com/" variant="dark" className="m-1" target="_blank" style={{ fontSize: "20px" }}>Visit Deep Blue</Button>
-                                <Button href="https://github.com/Chasetopher/deep-blue" variant="dark" target="_blank" style={{ fontSize: "20px" }}>See Github Repo</Button>
+                                <Button href="https://only-bans.herokuapp.com/" variant="dark" className="m-1" target="_blank" style={{ fontSize: "20px" }}>Visit Only Bans</Button>
+                                <Button href="https://github.com/zacktmiller92/only-bans" variant="dark" target="_blank" style={{ fontSize: "20px" }}>See Github Repo</Button>
                             </Container>
                         </Card.Body>
                     </Card>
@@ -99,59 +96,3 @@ function Portfolio() {
 export default Portfolio
 
 
-
-
-// <Row>
-// <Col md={6}>
-//     <Image src={deepBlue} thumbnail style={{ height: '500px', width: '100%' }} ></Image>
-//     <h4 className="text-center" style={{ fontFamily: "monospace" }}>Deep Blue</h4>
-//     <Container className="text-center mb-2">
-//         <Button href="https://deep-blue-project-2.herokuapp.com/" variant="dark" className="m-1" target="_blank">Visit Deep Blue</Button>
-//         <Button href="https://github.com/Chasetopher/deep-blue" variant="dark" target="_blank">See Github Repo</Button>
-//     </Container>
-// </Col>
-// <Col md={6}>
-//     <Image src={NPT} thumbnail style={{ height: '500px', width: '100%' }} ></Image>
-//     <h4 className="text-center" style={{ fontFamily: "monospace" }}>National Park Trip Planner</h4>
-//     <Container className="text-center mb-2">
-//         <Button href="https://miraj00.github.io/National-Park-Trip-Planner/" variant="dark" className="m-1" target="_blank">Visit National Park Trip Planner</Button>
-//         <Button href="https://github.com/miraj00/National-Park-Trip-Planner" variant="dark" target="_blank">See Github Repo</Button>
-//     </Container>
-// </Col>
-// </Row>
-// <Row>
-// <Col md={6}>
-//     <Image src={techBlog} thumbnail style={{ height: '500px', width: '100%' }} ></Image>
-//     <h4 className="text-center" style={{ fontFamily: "monospace" }}>Techno Blog</h4>
-//     <Container className="text-center mb-2">
-//         <Button href="https://rromero-techno-blog.herokuapp.com/" variant="dark" className="m-1" target="_blank">Visit Techno Blog</Button>
-//         <Button href="https://github.com/ronnieve23/Techno-Blog" variant="dark" target="_blank">See Github Repo</Button>
-//     </Container>
-// </Col>
-// <Col md={6}>
-//     <Image src={weather} thumbnail style={{ height: '500px', width: '100%' }} ></Image>
-//     <h4 className="text-center" style={{ fontFamily: "monospace" }}>Weather LookUp</h4>
-//     <Container className="text-center mb-2">
-//         <Button href="https://ronnieve23.github.io/WeatherLookUp/" variant="dark" className="m-1" target="_blank">Visit Weather LookUp</Button>
-//         <Button href="https://ronnieve23.github.io/WeatherLookUp/" variant="dark" target="_blank">See Github Repo</Button>
-//     </Container>
-// </Col>
-// </Row>
-// <Row style={{paddingBottom:'50px'}}>
-// <Col md={6}>
-//     <Image src={workDayPlanner} thumbnail style={{ height: '500px', width: '100%' }} ></Image>
-//     <h4 className="text-center" style={{ fontFamily: "monospace" }}>WorkDay Planner</h4>
-//     <Container className="text-center mb-2">
-//         <Button href="https://ronnieve23.github.io/WorkDayPlanner/" variant="dark" className="m-1" target="_blank">Visit WorkDay Planner</Button>
-//         <Button href="https://github.com/ronnieve23/WorkDayPlanner" variant="dark" target="_blank">See Github Repo</Button>
-//     </Container>
-// </Col>
-// <Col md={6}>
-//     <Image src={noteTaker} thumbnail style={{ height: '500px', width: '100%' }} ></Image>
-//     <h4 className="text-center" style={{ fontFamily: "monospace" }}>MyNotes</h4>
-//     <Container className="text-center mb-2">
-//         <Button href="https://mynotes-rromero.herokuapp.com/" variant="dark" className="m-1" target="_blank">Visit MyNotes</Button>
-//         <Button href="https://github.com/ronnieve23/MyNotes" variant="dark" target="_blank">See Github Repo</Button>
-//     </Container>
-// </Col>
-// </Row>
